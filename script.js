0e08c68bd1177d8aa4041d9f08852e4297d64803
Danielle Cutler <cutlerd02@gmail.com>
2020-11-11 21:40:25 -0700

    updated

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// code that is within this function will run after the entire page is ready
-$(document).ready(function() {
+function workPlanner() {
+    var currentDay = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-// this shows current day and time
-$("#currentDay").text(moment().format ("MMMM Do YYYY, h:mm:ss a"));
 
-$(".saveBtn").on("click", function () {
-    // values
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    console.log(this);
-    
-    
-   localStorage.setItem(value, time);
-//  uses local storage to save items
+moment().format("MMMM Do YYYY, h:mm:ss a");
+    var date =newDate();
+    var hours = date.getHours();
 
-})
+
+
+
+
+
 
